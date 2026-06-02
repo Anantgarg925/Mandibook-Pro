0037ad01ee0e180396404347dc01adbeb28052a0
Anant Garg <anantgarg.1999@gmail.com>
2026-06-02 15:00:19 +0530

    Delete supabase directory

--- supabase/migrations/20260526000000_allow_agent_stock_inquiries.sql
+++
@@ -1,3 +0,0 @@
-alter table public.inquiries
-  alter column truck_id drop not null;
-
--- supabase/migrations/20260601_add_preferred_payment_mode.sql
+++
@@ -1,3 +0,0 @@
--- Add preferred_payment_mode column to buyers table
-ALTER TABLE public.buyers
-ADD COLUMN preferred_payment_mode TEXT DEFAULT 'CASH' CHECK (preferred_payment_mode IN ('CASH', 'UPI', 'UDHAARI', 'CHEQUE', 'PENDING'));
